a97307251a8d6067c707db3d37053154742f76c4
SmrutiHegde <132127258+SmrutiHegde@users.noreply.github.com>
2024-09-18 12:11:17 +0530

    Update streamlit_app.py
    to a\call rest API's

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ if ingredients_list:
     
     for fruit_chosen  in ingredients_list:
         ingredients_string+= fruit_chosen+ ''
-    # st.write(ingredients_string)
              
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -50,6 +49,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered', icon="✅")
-    # if ingredients_string:
-    #     session.sql(my_insert_stmt).collect()
-    #     st.success('Your Smoothie is ordered!', icon="✅")
+   
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
